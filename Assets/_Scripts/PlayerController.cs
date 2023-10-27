049c253ac5837bf7734957d5176d64ac279d8fa8
giorgi25k <giorgi25k@gmail.com>
2023-10-28 00:03:26 +0400

    Implemented Animator Blend tree for movement and idle

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
     [SerializeField] private float _speed = 2f;
 
     private Rigidbody2D _rigidbody2d;
     private Vector2 _motionVector;
+
+    private static int ANIMATOR_HEX_HORIZONTAL = Animator.StringToHash("horizontal");
+    private static int ANIMATOR_HEX_VERTICAL = Animator.StringToHash("vertical");
 
     void Awake()
     {
@@ -21,6 +25,9 @@ public class PlayerController : MonoBehaviour
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
             );
+
+        _animator.SetFloat(ANIMATOR_HEX_HORIZONTAL, _motionVector.x);
+        _animator.SetFloat(ANIMATOR_HEX_VERTICAL, _motionVector.y);
     }
 
     private void FixedUpdate()
